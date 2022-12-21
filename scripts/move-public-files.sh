0274d38d2c935b988572929550310cdf461829c0
Yann Braga <yannbf@gmail.com>
2022-12-21 11:11:48 +0100

    add missing bash script

---
+++ scripts/move-public-files.sh
@@ -0,0 +1,9 @@
+
+#!/bin/bash
+
+if [[ -n $NEXT_PUBLIC_BASE_PATH ]]; then
+  DIRECTORY=${NEXT_PUBLIC_BASE_PATH/\//''}
+  cd public
+  mkdir $DIRECTORY
+  mv * $DIRECTORY || echo "move public files to /$DIRECTORY";
+fi
