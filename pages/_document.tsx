d3c71448af7915ea640ebb29006e53d6ab472360
winkerVSbecks <varunvachhar@gmail.com>
2023-01-26 14:54:42 -0500

    add og image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,25 +3,22 @@ import { global } from '@storybook/design-system';
 
 const { fontUrl } = global;
 
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
+export const getBasePathedSrc = (src: string) => (basePath && /^\//.test(src) ? `${basePath}${src}` : src);
+
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
           <meta name="title" content="Storybook status" />
-          <meta
-            name="description"
-            content="Daily CI checks for all officially supported framework integrations on Storybook."
-          />
+          <meta name="description" content="Daily CI checks for all officially supported framework integrations on Storybook." />
 
           <meta property="og:type" content="website" />
           <meta property="og:url" content="https://storybook.js.org/status/" />
           <meta property="og:title" content="Storybook status" />
-          <meta
-            property="og:description"
-            content="Daily CI checks for all officially supported framework integrations on Storybook."
-          />
-          <meta property="og:image" content="https://storybook.js.org/images/social/open-graph.png" />
+          <meta property="og:description" content="Daily CI checks for all officially supported framework integrations on Storybook." />
+          <meta property="og:image" content={getBasePathedSrc('/images/og-status.jpg')} />
 
           <meta property="twitter:card" content="summary_large_image" />
           <meta property="twitter:url" content="https://storybook.js.org/status/" />
@@ -30,12 +27,12 @@ class MyDocument extends Document {
             property="twitter:description"
             content="Daily CI checks for all officially supported framework integrations on Storybook."
           />
-          <meta property="twitter:image" content="https://storybook.js.org/images/social/open-graph.png" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/status/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/status/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/status/favicon-16x16.png" />
-          <link rel="manifest" href="/status/site.webmanifest" />
-          <link rel="shortcut icon" href="/status/favicon.svg" />
+          <meta property="twitter:image" content={getBasePathedSrc('/images/og-status.jpg')} />
+          <link rel="apple-touch-icon" sizes="180x180" href={getBasePathedSrc('/images//apple-touch-icon.png')} />
+          <link rel="icon" type="image/png" sizes="32x32" href={getBasePathedSrc('/images/favicon-32x32.png')} />
+          <link rel="icon" type="image/png" sizes="16x16" href={getBasePathedSrc('/images/favicon-16x16.png')} />
+          <link rel="manifest" href={getBasePathedSrc('/images/site.webmanifest')} />
+          <link rel="shortcut icon" href={getBasePathedSrc('/images/favicon.svg')} />
           <link rel="preconnect" href="https://fonts.gstatic.com/" crossOrigin="true" />
           <link href={fontUrl} rel="stylesheet" />
         </Head>
