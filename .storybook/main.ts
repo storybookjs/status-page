3d99c7e5cb0cef93fb722f39605b031ee96b78a3
Yann Braga <yannbf@gmail.com>
2023-07-28 09:07:03 +0200

    upgrade storybook packages to 7.1

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,18 +1,22 @@
 import type { StorybookConfig } from '@storybook/nextjs';
-
 const config: StorybookConfig = {
   stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-a11y', 'storybook-addon-designs'],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-a11y', '@storybook/addon-designs'],
   framework: {
     name: '@storybook/nextjs',
     options: {},
   },
   docs: {
-    docsPage: true,
+    autodocs: 'tag',
   },
-  features: {
-    interactionsDebugger: true,
+  webpackFinal: async (config) => {
+    config.resolve = config.resolve || {};
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      // Mock assert to avoid issues like Cannot read properties of undefined (reading 'custom')
+      assert: false,
+    };
+    return config;
   },
 };
-
 export default config;
