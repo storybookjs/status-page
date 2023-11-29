d1ffe55df7d3a10b7d1417a0481eed10b7687634
Yann Braga <yannbf@gmail.com>
2023-11-29 11:43:43 +0100

    increase timeout

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  staticPageGenerationTimeout: 300,
 };
 
 module.exports = nextConfig;
